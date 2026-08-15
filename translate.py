f4f65a263d9bb8d451b345d866dbf74778bb9e86
HASAN8620 <freefire.my.at@gmail.com>
2026-08-15 10:11:18 +0500

    Update translate.py

--- translate.py
+++ translate.py
@@ -149,7 +149,7 @@ def translate_batch(batch_dict):
 # 5. CORE LOGIC & RESUME SYSTEM
 # ==========================================
 if not os.path.exists(INPUT_FILE):
-    print(f"❌ ERROR: '{INPUT_FILE}' file nahi mili.")
+    print("\n⚠️ ALERT: Saari Groq Keys thak chuki hain. Progress save karne ke liye gracefully exit kar rahe hain!")
     sys.exit(0)
 
 print(f"📁 Reading source file: {INPUT_FILE}", flush=True)
